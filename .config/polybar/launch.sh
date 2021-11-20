deca51c5584fb281fd9eff461386a0727d4efbfe
pakkimz <pakkimz@gmail.com>
2021-11-20 19:48:16 +0700

    make more modular

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,6 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 # polybar bar1 >>/tmp/polybar1.log 2>&1 &
-polybar example >>/tmp/polybar2.log 2>&1 &
+polybar main >>/tmp/polybar2.log 2>&1 &
 
 echo "Bars launched..."

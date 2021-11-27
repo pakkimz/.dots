5b9e181ae0329079f41821839512eb93b758e547
pakkimz <pakkimz@gmail.com>
2021-11-27 11:10:05 +0700

    emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,375 @@
+;; ----------------------------------------------------------------------------------
+;; General
+;; ----------------------------------------------------------------------------------
+;; Set default font
+(set-face-attribute 'default nil
+                    :family "Hack"
+                    :height 102
+                    :weight 'bold
+                    :width 'normal)
+
+;; Transparency
+(set-frame-parameter (selected-frame) 'alpha '(95 95))
+(add-to-list 'default-frame-alist '(alpha 95 95))
+
+(setq delete-by-moving-to-trash t)              ;; move to trash
+(setq-default frame-title-format '("%f"))       ;; title
+; (setq-default mode-line-format nil)           ;; disable modeline
+; (setq default-directory "~/")                   ;; set HOME as default
+
+(show-paren-mode t)                   ;; highlight match pair
+(global-display-line-numbers-mode)    ;; display number
+(global-visual-line-mode t)           ;; line wrap
+
+(setq-default tab-width 2)            ;; tab width
+(setq-default indent-tabs-mode nil)   ;; expandtab
+(setq c-basic-offset 2)
+(setq sh-basic-offset 2)
+(setq js-indent-level 2)
+(setq css-indent-offset 2)
+
+(blink-cursor-mode 0)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode nil)
+
+; (setq visible-bell 1)
+(setq show-help-function nil)
+(setq initial-scratch-message nil)
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+(setq inhibit-startup-screen t)
+
+;; Smooth scrolling
+(setq redisplay-dont-pause t
+      scroll-margin 1
+      scroll-step 1
+      scroll-conservatively 10000
+      scroll-preserve-screen-position 1)
+
+;; Disable backup and autosave directories
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+(setq create-lockfiles nil)
+;; Enable backup and autosave directories
+; (setq backup-by-copying t)
+; (setq temporary-file-directory "~/.emacs.d/.tmp/")
+; (setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+; (setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+
+;; Backspace remove according to tab size
+(global-set-key [backspace] 'backspace-whitespace-to-tab-stop)
+
+;; Removes trailing whitespace when save
+(add-hook 'write-file-hooks 'delete-trailing-whitespace)
+
+;; No more typing the whole yes or no. Just y or n will do.
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Forces the messages to 0, and kills the *Messages* buffer - thus disabling it on startup.
+(setq-default message-log-max nil)
+(kill-buffer "*Messages*")
+;; ----------------------------------------------------------------------------------
+;; Packages
+;; ----------------------------------------------------------------------------------
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(setq use-package-always-ensure t)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile (require 'use-package))
+
+;; Autocompletion
+(use-package company
+  :config
+  (setq company-idle-delay 0)             ;; no delay
+  (setq company-minimum-prefix-length 2)  ;; suggestion after two character
+  (setq company-selection-wrap-around t)  ;; cycle through
+  :init
+  (add-hook 'after-init-hook 'global-company-mode))
+(provide 'init-company)
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "C-e") #'company-abort)
+  (define-key company-active-map (kbd "C-h") #'backward-delete-char)
+  (define-key company-active-map (kbd "C-y") #'company-complete-selection)
+  (define-key company-active-map (kbd "C-w") 'evil-delete-backward-word))
+
+;; Auto close pair
+(electric-pair-mode 1)
+;; Disable auto-indent
+; (when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
+;; Auto close single-quote and backtic
+(push '(?\' . ?\') electric-pair-pairs)
+(push '(?\` . ?\`) electric-pair-pairs)
+; (push '(?\' . ?\') electric-pair-text-pairs)    ;; in comment and text
+; (push '(?\` . ?\`) electric-pair-text-pairs)
+;; Disable pair in <
+(setq electric-pair-inhibit-predicate
+      `(lambda (c)
+         (if (char-equal c ?\<) t (,electric-pair-inhibit-predicate c))))
+;; Disable indent on html
+(defun remove-electric-indent-mode ()
+  (electric-indent-local-mode -1))
+(add-hook 'html-mode-hook 'remove-electric-indent-mode)
+
+;; Recent file or mru
+(use-package recentf
+             :config
+             (recentf-mode 1)
+             (setq recentf-max-menu-items 25))
+
+;; Ivy
+(use-package ivy
+             :diminish (ivy-mode . "")
+             :init (ivy-mode 1) ; globally at startup
+             :config
+             (setq ivy-use-virtual-buffers t)
+             (setq enable-recursive-minibuffers t)
+             (setq ivy-count-format "%d/%d "))
+(provide 'init-ivy)
+(with-eval-after-load 'ivy
+                      (global-set-key "\C-s" 'swiper)
+                      (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+                      (global-set-key (kbd "C-x C-b") 'ivy-switch-buffer)
+                      (global-set-key (kbd "C-x C-m") 'counsel-recentf)
+                      (global-set-key (kbd "\C-w") 'counsel-up-directory))
+
+(use-package undo-fu)
+
+(use-package evil
+             :demand t
+             :config
+             (setq evil-want-keybinding nil)
+             (setq evil-undo-system 'undo-fu)          ;; use undo-fu for system undo
+             (setq-default evil-shift-width 2)         ;; set shiftwidth when >>
+             (evil-set-leader 'normal (kbd "SPC"))     ;; leader key
+             (evil-mode 1))
+
+(use-package evil-collection
+             :after evil
+             (setq evil-want-integration t)
+             (evil-collection-init))
+
+(use-package evil-commentary
+             :after evil
+             :config
+             (evil-commentary-mode))
+
+(use-package evil-surround
+             :after evil
+             :config
+             (global-evil-surround-mode 1))
+
+(with-eval-after-load 'evil-maps
+                      (define-key evil-normal-state-map "Y" 'djoyner/copy-to-end-of-line)
+                      (define-key evil-visual-state-map (kbd ">") 'djoyner/evil-shift-right-visual)
+                      (define-key evil-visual-state-map (kbd "<") 'djoyner/evil-shift-left-visual)
+                      (define-key evil-insert-state-map (kbd "\C-h") 'delete-backward-char)
+                      (define-key evil-normal-state-map "\C-j" 'move-line-down)
+                      (define-key evil-normal-state-map "\C-k" 'move-line-up)
+                      (define-key evil-normal-state-map "\C-l" 'next-buffer)
+                      (define-key evil-normal-state-map "\C-h" 'previous-buffer)
+                      (define-key evil-normal-state-map (kbd "g.") 'goto-last-change)
+                      (define-key evil-normal-state-map (kbd "<leader>O") 'only-current-buffer)
+                      (define-key evil-normal-state-map (kbd "<leader>w") 'save-buffer)
+                      (define-key evil-normal-state-map (kbd "<leader>r") 'kill-this-buffer)
+                      (define-key evil-normal-state-map (kbd "<leader>q") 'save-buffers-kill-terminal)
+                      (define-key evil-insert-state-map (kbd "C-SPC") 'evil-force-normal-state)
+                      (define-key evil-normal-state-map (kbd "C-SPC") 'evil-ex-nohighlight)
+                      (define-key evil-visual-state-map (kbd "C-SPC") 'evil-force-normal-state)
+                      (define-key evil-ex-completion-map (kbd "C-SPC") (kbd "C-c"))
+                      (define-key evil-insert-state-map "\C-a" 'beginning-of-line)
+                      (define-key evil-insert-state-map "\C-e" 'end-of-line)
+                      (define-key evil-insert-state-map (kbd "TAB") 'tab-to-tab-stop)
+                      (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+                      (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+                      (define-key evil-normal-state-map "u" 'undo-fu-only-undo)
+                      (define-key evil-normal-state-map "\C-r" 'undo-fu-only-redo))
+
+(use-package neotree
+             :config
+             (setq neo-theme 'icon)
+             (setq-default neo-show-hidden-files t)
+             (global-set-key [f1] 'neotree-toggle)
+             (global-set-key [f2] 'my-neotree-project-dir-toggle)      ;; find directory
+             :init
+             (add-hook 'neo-after-create-hook
+                       (lambda (&rest _) (display-line-numbers-mode -1)))
+             (add-hook 'neotree-mode-hook
+                       (lambda ()
+                         (define-key evil-normal-state-local-map (kbd "s") 'neotree-enter-vertical-split)
+                         (define-key evil-normal-state-local-map (kbd "i") 'neotree-enter-horizontal-split)
+                         (define-key evil-normal-state-local-map (kbd "o") 'neotree-enter)
+                         (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter))))
+
+;; Org mode
+(use-package org-bullets
+             :config
+             ; (setq org-adapt-indentation nil)                ;; disable indent
+             (setq org-bullets-bullet-list '("•" "➤" "•"))
+             (global-set-key "\C-ca" 'org-agenda)
+             :init
+             (add-hook 'org-mode-hook 'org-bullets-mode 1))
+
+(use-package jbeans-theme
+             :config
+             (load-theme 'jbeans t))
+
+;; Web mode
+(use-package company-web)
+(use-package php-mode)
+(use-package ac-html-bootstrap)
+(use-package web-mode
+             :diminish "🌎"
+             :mode
+             (("\\.phtml\\'" . web-mode)
+              ("\\.tpl\\.php\\'" . web-mode)
+              ("\\.jsp\\'" . web-mode)
+              ("\\.as[cp]x\\'" . web-mode)
+              ("\\.erb\\'" . web-mode)
+              ("\\.mustache\\'" . web-mode)
+              ("\\.djhtml\\'" . web-mode)
+              ("\\.jst.ejs\\'" . web-mode)
+              ("\\.html?\\'" . web-mode))
+             :init
+             (setq web-mode-enable-block-face t)
+             (setq web-mode-enable-comment-keywords t)
+             (setq web-mode-enable-current-element-highlight t)
+             (setq web-mode-enable-current-column-highlight t)
+             (setq web-mode-script-padding 2)
+             (setq web-mode-style-padding 2)
+             (setq web-mode-comment-style 2)
+             (setq web-mode-code-indent-offset 2)
+             (setq web-mode-markup-indent-offset 2))
+
+;; ----------------------------------------------------------------------------------
+;; Functions
+;; ----------------------------------------------------------------------------------
+;; Close all buffer except this buffer
+(defun only-current-buffer ()
+  (interactive)
+  (mapc 'kill-buffer (cdr (buffer-list (current-buffer)))))
+
+;; Copy to the end of line
+(defun djoyner/copy-to-end-of-line ()
+  (interactive)
+  (copy-region-as-kill (point) (point-at-eol)))
+
+;; C-u to delete line before cursor
+(define-key evil-insert-state-map (kbd "C-u")
+            (lambda ()
+              (interactive)
+              (if (looking-back "^" 0)
+                (backward-delete-char 1)
+                (if (looking-back "^\s*" 0)
+                  (delete-region (point) (line-beginning-position))
+                  (evil-delete (+ (line-beginning-position) (current-indentation)) (point))))))
+
+;; Overload shifts so that they don't lose the selection
+(defun djoyner/evil-shift-left-visual ()
+  (interactive)
+  (evil-shift-left (region-beginning) (region-end))
+  (evil-normal-state)
+  (evil-visual-restore))
+(defun djoyner/evil-shift-right-visual ()
+  (interactive)
+  (evil-shift-right (region-beginning) (region-end))
+  (evil-normal-state)
+  (evil-visual-restore))
+
+;; Move line up and down
+(defun move-line-up ()
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+(defun move-line-down ()
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+;; Backspace remove as one ta when there is no character
+(defvar my-offset 2 "My indentation offset. ")
+(defun backspace-whitespace-to-tab-stop ()
+  "Delete whitespace backwards to the next tab-stop, otherwise delete one character."
+  (interactive)
+  (if (or indent-tabs-mode
+          (region-active-p)
+          (save-excursion
+            (> (point) (progn (back-to-indentation)
+                              (point)))))
+    (call-interactively 'backward-delete-char-untabify)
+    (let ((movement (% (current-column) my-offset))
+          (p (point)))
+      (when (= movement 0) (setq movement my-offset))
+      ;; Account for edge case near beginning of buffer
+      (setq movement (min (- p 1) movement))
+      (save-match-data
+        (if (string-match "[^\t ]*\\([\t ]+\\)$" (buffer-substring-no-properties (- p movement) p))
+          (backward-delete-char (- (match-end 1) (match-beginning 1)))
+          (call-interactively 'backward-delete-char))))))
+
+;; Rename file and buffer
+;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+      (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+        (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
+;; Neotree cd directory function
+(defun my-neotree-project-dir-toggle ()
+  "Open NeoTree using the project root, using projectile, find-file-in-project,
+  or the current buffer directory."
+  (interactive)
+  (require 'neotree)
+  (let* ((filepath (buffer-file-name))
+         (project-dir
+           (with-demoted-errors
+             (cond
+               ((featurep 'projectile)
+                (projectile-project-root))
+               ((featurep 'find-file-in-project)
+                (ffip-project-root))
+               (t ;; Fall back to version control root.
+                 (if filepath
+                   (vc-call-backend
+                     (vc-responsible-backend filepath) 'root filepath)
+                   nil)))))
+         (neo-smart-open t))
+
+    (if (and (fboundp 'neo-global--window-exists-p)
+             (neo-global--window-exists-p))
+      (neotree-hide)
+      (neotree-show)
+      (when project-dir
+        (neotree-dir project-dir))
+      (when filepath
+        (neotree-find filepath)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(tern-auto-complete lsp-mode counsel use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

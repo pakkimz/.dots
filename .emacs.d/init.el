d6993e9955ec10ce13526045bde28d443408ba43
pakkimz <pakkimz@gmail.com>
2021-11-28 12:48:30 +0700

    emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -229,16 +229,25 @@
              :mode
              (("\\.css\\'" . web-mode)
               ("\\.html?\\'" . web-mode)
-              ("\\.php\\'" . web-mode)
-              ("\\.ejs$\\'" . web-mode))
+              ("\\.php\\'" . web-mode))
              :config
              (setq web-mode-markup-indent-offset 2)
              (setq web-mode-css-indent-offset 2)
              (setq web-mode-code-indent-offset 2)
              (setq web-mode-enable-auto-closing t)
+             (setq web-mode-ac-sources-alist
+                   '(("css" . (ac-source-words-in-buffer ac-source-css-property))
+                     ("html" . (ac-source-words-in-buffer ac-source-abbrev))
+                     ("php" . (ac-source-words-in-buffer
+                                ac-source-words-in-same-mode-buffers
+                                ac-source-dictionary))))
              (setq web-mode-engines-alist
                    '(("php"    . "\\.html\\'")
                      ("blade"  . "\\.blade\\."))))
+
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-20201213.1255/dict/")
+(ac-config-default)
+
 ;; Js2 mode
 (use-package js2-mode
              :init

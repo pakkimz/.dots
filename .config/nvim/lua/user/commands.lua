e1aca1d7502dff7439bce164f1a8e43793ee3c98
pakkimz <pakkimz@gmail.com>
2022-01-26 12:30:42 +0700

    custom commands

---
+++ .config/nvim/lua/user/commands.lua
@@ -0,0 +1,22 @@
+-- Treat for every no name file as text file
+vim.cmd [[
+  augroup noname
+    autocmd BufEnter * if &filetype == "" | setlocal ft=text | endif
+  augroup end
+]]
+
+-- Highlighted yank
+vim.cmd [[
+  augroup highlight_yank
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=100 }
+  augroup end
+]]
+
+-- When switching buffers, preserve window view.
+vim.cmd [[
+  augroup preserve
+    autocmd BufLeave * if !&diff | let b:winview = winsaveview() | endif
+    autocmd BufEnter * if exists('b:winview') && !&diff | call winrestview(b:winview) | unlet! b:winview | endif
+  augroup end
+]]

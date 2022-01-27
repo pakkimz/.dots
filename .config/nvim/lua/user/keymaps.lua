b76d20a62d3a4fabe8cc14e4154198a3d63961ba
pakkimz <pakkimz@gmail.com>
2022-01-27 22:02:09 +0700

    make search use normal regex

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -67,6 +67,12 @@ keymap("n", "k", "v:count ? 'k' : 'gk'", exprs)
 keymap("c", "<c-n>", "wildmenumode() ? '<c-n>' : '<down>'", exprs)
 keymap("c", "<c-p>", "wildmenumode() ? '<c-p>' : '<up>'", exprs)
 
+-- Make search use normal regex
+keymap("n", "/", "/\\v", opts)
+keymap("v", "/", "/\\v", opts)
+keymap("n", "?", "?\\v", opts)
+keymap("v", "?", "?\\v", opts)
+
 -- Increase value resize split
 keymap("n", "<C-w>+", ":resize +5<CR>", optst)
 keymap("n", "<C-w>-", ":resize -5<CR>", optst)
@@ -79,12 +85,6 @@ keymap("v", "<C-space>", "<Esc>gV", opts)
 keymap("x", "<C-space>", "<Esc>", opts)
 keymap("c", "<C-space>", "<C-c>", opts)
 keymap("n", "<C-space>", "<Esc>:noh<CR>", opts)
-
--- Make search use normal regex
-keymap("n", "/", "/\\v", opts)
-keymap("v", "/", "/\\v", opts)
-keymap("n", "?", "?\\v", opts)
-keymap("v", "?", "?\\v", opts)
 
 -- Moving around
 keymap("i", "<C-a>", "<Home>", opts)

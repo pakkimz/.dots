2709d01a6776b1130f9548261f39f7eb4b20c098
pakkimz <pakkimz@gmail.com>
2022-02-01 16:10:24 +0700

    remove sumneko_lua

--- .config/nvim/lua/user/lsp/lsp-installer.lua
+++ .config/nvim/lua/user/lsp/lsp-installer.lua
@@ -16,11 +16,6 @@ lsp_installer.on_server_ready(function(server)
 	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
 	 end
 
-	 if server.name == "sumneko_lua" then
-	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	 end
-
 	 if server.name == "pyright" then
 	 	local pyright_opts = require("user.lsp.settings.pyright")
 	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
@@ -30,4 +25,3 @@ lsp_installer.on_server_ready(function(server)
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
 end)
-

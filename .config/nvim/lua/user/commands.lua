de73c1750c828ab2287d6a04220842fcfaeb8705
pakkimz <pakkimz@gmail.com>
2022-02-17 02:49:05 +0700

    perserve

--- .config/nvim/lua/user/commands.lua
+++ .config/nvim/lua/user/commands.lua
@@ -46,9 +46,9 @@ vim.cmd [[
 ]]
 
 -- When switching buffers, preserve window view.
-vim.cmd [[
-  augroup preserve
-    autocmd BufLeave * if !&diff | let b:winview = winsaveview() | endif
-    autocmd BufEnter * if exists('b:winview') && !&diff | call winrestview(b:winview) | unlet! b:winview | endif
-  augroup end
-]]
+-- vim.cmd [[
+--   augroup preserve
+--     autocmd BufLeave * if !&diff | let b:winview = winsaveview() | endif
+--     autocmd BufEnter * if exists('b:winview') && !&diff | call winrestview(b:winview) | unlet! b:winview | endif
+--   augroup end
+-- ]]

69a3bdd3669854d39e63dac9b8a84ff25e9f50ec
pakkimz <pakkimz@gmail.com>
2022-08-26 23:18:03 +0700

    sumneko lua settings

---
+++ .config/nvim/lua/user/lsp/settings/sumneko_lua.lua
@@ -0,0 +1,18 @@
+return {
+  settings = {
+    telemetry = {
+      enable = false,
+    },
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
+        },
+      },
+    },
+  },
+}
